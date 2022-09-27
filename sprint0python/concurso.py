0ea93a1a6a77e9c3a0b8dd666b27c1cf19eb2484
Pablo Hermida <phermidaa@fpcoruna.afundacion.org>
2022-09-27 09:40:51 +0200

    Ejercicio 1

---
+++ sprint0python/concurso.py
@@ -0,0 +1,19 @@
+print("¿Cuántas asignaturas tenemos?")
+print("Opción A: Cinco asignaturas.")
+print("Opción B: Seis asignaturas.")
+print("Opción C: Siete asignaturas.")
+
+respuesta = input()
+
+if respuesta == 'a':
+    print("Respuesta incorrecta")
+elif respuesta == 'A':
+    print("Respuesta incorrecta")
+elif respuesta == 'b':
+    print("Respuesta correcta")
+elif respuesta == 'B':
+    print("Respuesta correcta")
+elif respuesta == 'c':
+    print("Respuesta incorrecta")
+elif respuesta == 'C':
+    print("Respuesta incorrecta")
