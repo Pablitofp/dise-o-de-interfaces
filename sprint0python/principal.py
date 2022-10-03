7772cbe5f7345bd2936cdbc47ff0b10525def6bd
Pablo Hermida <phermidaa@fpcoruna.afundacion.org>
2022-10-03 16:44:30 +0200

    Ejercicio 6

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,9 +1,10 @@
 from fibonacci import funcion_fibonacci
 from fibonacci2 import funcion_fibonacci2
+import time
 
 print()
 
-num = int(input("Introduzca numero: "))
+num = int(input("Introduzca número: "))
 
 print()
 
@@ -16,8 +17,15 @@ op = input("Introduzca opción: ")
 print()
 
 if op == 'A' or op == 'a':
-    print(funcion_fibonacci2(num))
-elif op == 'B' or op == 'b':
+    start_time = time.time()
     print(funcion_fibonacci(num))
-
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+elif op == 'B' or op == 'b':
+    start_time = time.time()
+    print(funcion_fibonacci2(num))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 print()
