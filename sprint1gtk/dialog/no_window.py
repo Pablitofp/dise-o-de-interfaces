8b5da46e01a2690f7472b8af674a6c0950593367
Pablo Hermida <phermidaa@fpcoruna.afundacion.org>
2022-10-16 22:52:38 +0200

    Ejercicio 3

---
+++ sprint1gtk/dialog/no_window.py
@@ -0,0 +1,10 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class NoWindow(Gtk.Window):
+    
+    def __init__(self):
+        label = Gtk.label(label = "No")
+        super().__init__ (title = "No")
+        self.add(label)
---
+++ sprint1gtk/dialog/yes_window.py
@@ -0,0 +1,10 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class YesWindow(Gtk.Window):
+    label = Gtk.label(label = "Yes")
+    
+    def __init__(self):
+        super().__init__ (title = "Yes")
+        self.add(self.label)
