b05ab064d4ba527b60178035fe2ba241e35c03d3
Pablo Hermida <phermidaa@fpcoruna.afundacion.org>
2022-11-12 19:06:53 +0100

    Ejercicio 2

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,13 +2,28 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CatalogActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        Button button = findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(context, "Arrancando otra actividad...", Toast.LENGTH_LONG).show(); //Comprobación de que el botón funciona
+                Intent myIntent = new Intent(context, DetailActivity.class);
+                context.startActivity(myIntent);
+            }
+        });
     }
 }
