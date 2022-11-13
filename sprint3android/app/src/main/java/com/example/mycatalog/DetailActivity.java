211b0417b87fa1a2ae0e0168d4dae65c6d523a62
Pablo Hermida <phermidaa@fpcoruna.afundacion.org>
2022-11-13 14:16:42 +0100

    Ejercicio 2 válido

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,13 +1,18 @@
 package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        //Toast.makeText(context, "Arrancando otra actividad...", Toast.LENGTH_LONG).show(); //Comprobación de que el botón funciona
     }
 }
